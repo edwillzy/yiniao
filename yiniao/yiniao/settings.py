6bf5a07ef8c7efb952df380958e965582fe35000
edwillzy <edwillzy@hotmail.com>
2021-02-23 23:27:23 +0800

    Update settings.py

--- yiniao/yiniao/settings.py
+++ yiniao/yiniao/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eg4fgz=(960u@%yvxb!i-!s2e@2@40=^mrvekvoqs^(b32$zw&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.157.130']
+ALLOWED_HOSTS = []
 
 
 # Application definition
